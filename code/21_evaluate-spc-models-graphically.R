5f98fe7fc9a4c9ac7671d530c58c326146ea9e01
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2020-12-11 14:50:28 +0100

    Comment (i.e. for drake workflow)

--- code/21_evaluate-spc-models-graphically.R
+++ code/21_evaluate-spc-models-graphically.R
@@ -14,46 +14,46 @@
 
 # Read model outputs -----------------------------------------------------------
 
-## 1: Soil properties in the group related to "Mineralogy":
-## Total Fe, total Si, total Al, total K, total Zn, total Cu,
-## total Mn
-pls_Fe_total <- readRDS("models/rep-kfold-cv/pls_Fe_total.Rds")
-pls_Si_total <- readRDS("models/rep-kfold-cv/pls_Si_total.Rds")
-pls_Al_total <- readRDS("models/rep-kfold-cv/pls_Al_total.Rds")
-pls_K_total <- readRDS("models/rep-kfold-cv/pls_K_total.Rds")
-pls_Ca_total <- readRDS("models/rep-kfold-cv/pls_Ca_total.Rds")
-pls_Zn_total <- readRDS("models/rep-kfold-cv/pls_Zn_total.Rds")
-pls_Cu_total <- readRDS("models/rep-kfold-cv/pls_Cu_total.Rds")
-pls_Mn_total <- readRDS("models/rep-kfold-cv/pls_Mn_total.Rds")
-## 2: Soil properties in the group related to
-## "Mineralogy/Plant Nutrition":
-## Sand, silt, clay; pH, exchangeable K, exchangeable Ca, exchangeable Mg,
-## exchangeable Al, CEC, base saturation
-pls_sand <- readRDS("models/rep-kfold-cv/pls_sand.Rds")
-pls_silt <- readRDS("models/rep-kfold-cv/pls_silt.Rds")
-pls_clay <- readRDS("models/rep-kfold-cv/pls_clay.Rds")
-pls_pH <- readRDS("models/rep-kfold-cv/pls_pH.Rds")
-pls_exch_K <- readRDS("models/rep-kfold-cv/pls_exch_K.Rds")
-pls_exch_Ca <- readRDS("models/rep-kfold-cv/pls_exch_Ca.Rds")
-pls_exch_Mg <- readRDS("models/rep-kfold-cv/pls_exch_Mg.Rds")
-pls_exch_Al <- readRDS("models/rep-kfold-cv/pls_exch_Al.Rds")
-pls_CEC <- readRDS("models/rep-kfold-cv/pls_CEC.Rds")
-pls_BS <- readRDS("models/rep-kfold-cv/pls_BS.Rds")
-## 3: Soil properties in the group related to
-## "Organic Matter":
-## Total C, total N, total S, total P
-pls_C <- readRDS("models/rep-kfold-cv/pls_C.Rds")
-pls_N <- readRDS("models/rep-kfold-cv/pls_N.Rds")
-pls_S <- readRDS("models/rep-kfold-cv/pls_S.Rds")
-pls_P <- readRDS("models/rep-kfold-cv/pls_P.Rds")
-## 4: Soil properties in the group related to
-## "Plant Nutrition":
-## Resin extractable P, DTPA Fe, DTPA Zn, DTPA Cu, DTPA Mn
-pls_resin_P_log <- readRDS("models/rep-kfold-cv/pls_resin_P_log.Rds")
-pls_Fe_DTPA_log <- readRDS("models/rep-kfold-cv/pls_Fe_DTPA_log.Rds")
-pls_Zn_DTPA <- readRDS("models/rep-kfold-cv/pls_Zn_DTPA.Rds")
-pls_Cu_DTPA <- readRDS("models/rep-kfold-cv/pls_Cu_DTPA.Rds")
-pls_Mn_DTPA <- readRDS("models/rep-kfold-cv/pls_Mn_DTPA.Rds")
+# ## 1: Soil properties in the group related to "Mineralogy":
+# ## Total Fe, total Si, total Al, total K, total Zn, total Cu,
+# ## total Mn
+# pls_Fe_total <- readRDS("models/rep-kfold-cv/pls_Fe_total.Rds")
+# pls_Si_total <- readRDS("models/rep-kfold-cv/pls_Si_total.Rds")
+# pls_Al_total <- readRDS("models/rep-kfold-cv/pls_Al_total.Rds")
+# pls_K_total <- readRDS("models/rep-kfold-cv/pls_K_total.Rds")
+# pls_Ca_total <- readRDS("models/rep-kfold-cv/pls_Ca_total.Rds")
+# pls_Zn_total <- readRDS("models/rep-kfold-cv/pls_Zn_total.Rds")
+# pls_Cu_total <- readRDS("models/rep-kfold-cv/pls_Cu_total.Rds")
+# pls_Mn_total <- readRDS("models/rep-kfold-cv/pls_Mn_total.Rds")
+# ## 2: Soil properties in the group related to
+# ## "Mineralogy/Plant Nutrition":
+# ## Sand, silt, clay; pH, exchangeable K, exchangeable Ca, exchangeable Mg,
+# ## exchangeable Al, CEC, base saturation
+# pls_sand <- readRDS("models/rep-kfold-cv/pls_sand.Rds")
+# pls_silt <- readRDS("models/rep-kfold-cv/pls_silt.Rds")
+# pls_clay <- readRDS("models/rep-kfold-cv/pls_clay.Rds")
+# pls_pH <- readRDS("models/rep-kfold-cv/pls_pH.Rds")
+# pls_exch_K <- readRDS("models/rep-kfold-cv/pls_exch_K.Rds")
+# pls_exch_Ca <- readRDS("models/rep-kfold-cv/pls_exch_Ca.Rds")
+# pls_exch_Mg <- readRDS("models/rep-kfold-cv/pls_exch_Mg.Rds")
+# pls_exch_Al <- readRDS("models/rep-kfold-cv/pls_exch_Al.Rds")
+# pls_CEC <- readRDS("models/rep-kfold-cv/pls_CEC.Rds")
+# pls_BS <- readRDS("models/rep-kfold-cv/pls_BS.Rds")
+# ## 3: Soil properties in the group related to
+# ## "Organic Matter":
+# ## Total C, total N, total S, total P
+# pls_C <- readRDS("models/rep-kfold-cv/pls_C.Rds")
+# pls_N <- readRDS("models/rep-kfold-cv/pls_N.Rds")
+# pls_S <- readRDS("models/rep-kfold-cv/pls_S.Rds")
+# pls_P <- readRDS("models/rep-kfold-cv/pls_P.Rds")
+# ## 4: Soil properties in the group related to
+# ## "Plant Nutrition":
+# ## Resin extractable P, DTPA Fe, DTPA Zn, DTPA Cu, DTPA Mn
+# pls_resin_P_log <- readRDS("models/rep-kfold-cv/pls_resin_P_log.Rds")
+# pls_Fe_DTPA_log <- readRDS("models/rep-kfold-cv/pls_Fe_DTPA_log.Rds")
+# pls_Zn_DTPA <- readRDS("models/rep-kfold-cv/pls_Zn_DTPA.Rds")
+# pls_Cu_DTPA <- readRDS("models/rep-kfold-cv/pls_Cu_DTPA.Rds")
+# pls_Mn_DTPA <- readRDS("models/rep-kfold-cv/pls_Mn_DTPA.Rds")
 
 ## Nest model output lists in one list
 models <- list(
@@ -93,24 +93,24 @@ predobs_cv <- extract_predobs(model_list = models)
 # Extract model evaluation statistics for plot annotations =====================
 
 # Extract model evaluation statistics (cross-validation only)
-stats_cv <- extract_stats(model_list = models)
+stats_cv_raw <- extract_stats(model_list = models)
 
-stats_cv_csv <- write_csv(x = stats_cv,
+stats_cv_csv <- write_csv(x = stats_cv_raw,
   path = here("out", "files", "yamsys-data-model-stats.csv"))
 
 ## Select only models for soil properties that have RPD_cv > 2 =================
 
-models_rpd_bigger2 <- stats_cv %>% 
+models_rpd_bigger2 <- stats_cv_raw %>% 
   filter(rpd > 2) %>%
   .$model
 
-stats_cv <- stats_cv %>%
+stats_cv <- stats_cv_raw %>%
   filter(rpd > 2)
 
 predobs_cv <- predobs_cv %>%
   filter(model %in% models_rpd_bigger2)
 # Check filtering
-unique(predobs_cv$model) # worked!
+# unique(predobs_cv$model) # worked!
 
 ## Reorder factor levels to realize custom panel order in plot =================
 
